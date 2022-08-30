a6d519dd3fe3355a06b595419bf2636a7ba30d09
zijiren233 <pyh1670605849@gmail.com>
2022-08-30 11:13:56 +0800

    Fix: 16bit color

--- backGround.go
+++ backGround.go
@@ -2,12 +2,13 @@ package text2picture
 
 import (
 	"image"
+	"image/color"
 	"image/draw"
 )
 
-func NewBackGroundWithColor(width, height int, color *image.Uniform) *image.RGBA {
+func NewBackGroundWithColor(width, height int, _16bit_color uint16) *image.RGBA {
 	rgba := image.NewRGBA(image.Rect(0, 0, width, height))
-	draw.Draw(rgba, rgba.Bounds(), color, image.Point{}, draw.Src)
+	draw.Draw(rgba, rgba.Bounds(), image.NewUniform(color.Gray16{_16bit_color}), image.Point{}, draw.Src)
 	return rgba
 }
 
